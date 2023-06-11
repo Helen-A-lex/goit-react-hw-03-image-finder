88449085c6c233d78ab20d893437ff288711ff0b
Helen-A-lex <elenahomutovskaa652@gmail.com>
2023-06-11 17:13:47 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,78 +17,74 @@ export class App extends Component {
     page: 1,
     isEmpty: false,
     isShownButton: false,
-    perPage: 12
+    perPage: 12,
   };
 
-  async componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps, prevState) {
     const { searchName, page } = this.state;
 
     if (prevState.searchName !== searchName || prevState.page !== page) {
       this.loadImages(searchName, page);
     }
-    
   }
-
 
   loadImages = async (searchName, page) => {
     this.setState({ isLoading: true, error: null });
     try {
-      const {hits, totalHits} = await API.getImages(searchName, page);
+      const { hits, totalHits } = await API.getImages(searchName, page);
       if (!hits.length) {
-        this.setState({ isEmpty: true })
+        this.setState({ isEmpty: true });
         return;
       }
-       this.setState(prevState => ({
+      this.setState(prevState => ({
         images: [...prevState.images, ...hits],
-         page,
-         isShownButton: page < Math.ceil(totalHits /this.state.perPage)
-         
+        page,
+        isShownButton: page < Math.ceil(totalHits / this.state.perPage),
       }));
-    }
-      catch (error) {
-        if (error.code !== 'ERR_CANCELED') {
-          this.setState({
-            error: 'Oops! Something went wrong! Try reloading the page!',
-          });
-        }
-      } finally {
-        this.setState({ isLoading: false });
+    } catch (error) {
+      if (error.code !== 'ERR_CANCELED') {
+        this.setState({
+          error: 'Oops! Something went wrong! Try reloading the page!',
+        });
       }
-}
-  
-
-  handleSearch = searchName => {
-    this.setState({ searchName, page: 1 });
+    } finally {
+      this.setState({ isLoading: false });
+    }
   };
 
-  
+  handleSearch = searchName => {
+    this.setState({
+      searchName,
+      images: [],
+      page: 1,
+      isShownButton: false,
+      isEmpty: false,
+      error: null,
+    });
+  };
+
   handleButtonLoadMore = () => {
     this.setState(prevState => ({
       page: prevState.page + 1,
-    }))
-  
-  }
-
-  
-  
-
+    }));
+  };
 
   render() {
-    const { images, isLoading, error,isEmpty, isShownButton} = this.state;
+    const { images, isLoading, error, isEmpty, isShownButton } = this.state;
     return (
       <Layout>
         <GlobalStyle />
         <Searchbar onSubmit={this.handleSearch} />
-        {isEmpty && <Text >Sorry. There are no images ... </Text>}
+        {isEmpty && <Text>Sorry. There are no images ... </Text>}
         {isLoading ? (
           <Loader />
         ) : images.length > 0 ? (
           <>
             <ImageGallery items={images} />
-             {isShownButton && <Button onClick={this.handleButtonLoadMore} />}
+            {isShownButton && <Button onClick={this.handleButtonLoadMore} />}
           </>
         ) : null}
-      
+
         {error && <ErrorMessage>{error}</ErrorMessage>}
         <ToastContainer autoClose={2000} />
       </Layout>
